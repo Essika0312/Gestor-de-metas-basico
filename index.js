410bd2940b62008f07fae75e3dc9f18ab2b511ec
Emanoel <emanoelfreitaskremser@gmail.com>
2024-09-11 23:28:13 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -61,9 +61,25 @@ const metasRealizadas = async () => {
   }
 
   await select({
-    message:"Metas Realizadas",
+    message:"Metas Realizadas " + realizadas.length,
     choices: [...realizadas]
   })
+}
+
+const metasAbertas = async () => {
+  const abertas = metas.filter((meta) => {
+    return meta.checked != true
+  })
+
+  if(abertas.length == 0) {
+    console.log("Não existe metas abertas! :)")
+    return
+  }
+
+  await select({
+    message: "Metas Abertas " + abertas.length,
+    choices: [...abertas]
+})
 }
 
 const start = async () => {
@@ -86,6 +102,10 @@ const start = async () => {
                 value: "realizadas"
               },
               {
+                name: "Metas abertas",
+                value: "abertas"
+              },
+              {
                 name: "Sair",
                 value: "sair"
               }
@@ -102,6 +122,9 @@ const start = async () => {
             break
           case "realizadas":
             await metasRealizadas()
+            break
+          case "abertas":
+            await metasAbertas()
             break
           case "sair":
             console.log("Até mais!")
